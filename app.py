969bc0058e9930b372c77d6021d82450d991381a
minuum <minwool0357@gmail.com>
2024-06-05 22:14:55 +0900

    update

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-# import ('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import os
 from glob import glob
