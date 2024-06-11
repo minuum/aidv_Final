af50b96a1c7bd9175b06ce7b0d716adcf361e1ae
minuum <minwool0357@gmail.com>
2024-06-11 22:44:26 +0900

    wqere

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def load_and_process_data():
     # embeddings은 OpenAI의 임베딩을 사용
     # vectordb는 chromadb사용함
     embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
-    vectordb = Chroma.from_documents(documents=chunks, embedding=embeddings)
+    vectordb = chromadb.from_documents(documents=chunks, embedding=embeddings)
     return vectordb
 
 vectordb = load_and_process_data()
@@ -139,17 +139,6 @@ if __name__ == '__main__':
         st.session_state["model"] = st.radio("모델을 선택해주세요.", ["gpt-4o", "gpt-3.5-turbo"])
         st.session_state["service"] = st.radio("답변 카테고리를 선택해주세요.", ["지식검색", "퀴즈"])
         st.session_state["prompt"] = update_prompt(st.session_state["service"])
-        st.write()
-        if st.session_state["service"] =="퀴즈":
-            st.markdown("###ㅋ")
-            with st.expander("지식 검색", expanded=False):
-                st.markdown('''
-                            #### 문제 입력
-                            - 주제만 입력해주세요
-                            - 예) 조지아, 그리스로마신화, 아르키메데스, 기묘한이야기....
-                            #### 정답 입력
-                            - 예) 1.a / 2.b / 3.b / 4.c / 5.c
-                            ''')
     chatbot = Chatbot(api_key=st.session_state["OPENAI_API"],
                        retriever=retriever,
                        sys_prompt=st.session_state["prompt"],
