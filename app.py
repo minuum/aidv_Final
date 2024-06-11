c39a0482a157bda792a307c1bdb45133956bc9d1
minuum <minwool0357@gmail.com>
2024-06-11 23:25:23 +0900

    qwrewqer

--- app.py
+++ app.py
@@ -230,18 +230,26 @@ if __name__ == '__main__':
                         us_dict[int(key)] = value
                     logging.warning("내가 낸 답변 :" +str(us_dict))
                 logging.warning("정답 :" +str(correct_answer))
+
+
                 for key in correct_answer.keys():
+
                 # 두 딕셔너리의 특정 키에 대한 값이 같은지 확인합니다.
                     if key in us_dict and correct_answer[key] == us_dict[key]:
                         logging.warning(str(key)+"번 정답!")
+                        with st.chat_message("ai"):
+                            st.markdown("정답입니다!")
+                        st.session_state.correct_answers += 1
+                        st.session_state.chat_history.append({"role": "user", "message": user_answer})
+                        st.session_state.chat_history.append({"role": "ai", "message": "정답입니다!"})
                     else:
                         logging.warning(str(key)+"번 오답!")
-                if any(user_answer.lower() == ans.lower() for ans in correct_answer.values()):
-                    with st.chat_message("ai"):
-                        st.markdown("정답입니다!")
-                    st.session_state.correct_answers += 1
-                    st.session_state.chat_history.append({"role": "user", "message": user_answer})
-                    st.session_state.chat_history.append({"role": "ai", "message": "정답입니다!"})
+                        with st.chat_message("ai"):
+                            st.markdown("정답입니다!")
+                        st.session_state.correct_answers += 1
+                        st.session_state.chat_history.append({"role": "user", "message": user_answer})
+                        st.session_state.chat_history.append({"role": "ai", "message": "정답입니다!"})
+                    
                 else:
                     with st.chat_message("ai"):
                         st.markdown("틀렸습니다. 다시 시도해보세요.")
